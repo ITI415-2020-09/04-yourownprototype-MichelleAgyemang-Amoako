18ee98fad1528ff527d9bb7d7392907513eb36a5
MichelleAgyemang-Amoako <68355116+MichelleAgyemang-Amoako@users.noreply.github.com>
2020-10-03 17:24:57 -0400

    Pathway Prototype
    I tried pushing all the stuff from my individual unity project I made, into this repository.

---
+++ Pathway/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text winTextObject;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winTextObject.GetComponent<Text>().text = "";
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Treasure"))
+        {
+            other.gameObject.SetActive(false);
+            winTextObject.GetComponent<Text>().text = "You Won";
+
+        }
+
+    }
+}
